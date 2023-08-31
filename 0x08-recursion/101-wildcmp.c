8817dc182ed15dde91b820bd79363a84e180ab6a
hesghani <hesghani@gmail.com>
2023-08-31 04:22:27 -0700

    hope1

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,20 +9,21 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s2 == '*')
-    {
-        if (*(s2 + 1) == '*' || *s1 == '\0')
-            return (wildcmp(s1, s2 + 1));
-        else
-            return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-    }
-    else if (*s1 == '\0' && *s2 == '\0')
-    {
-        return (1);
-    }
-    else if (*s1 == *s2)
-    {
-        return (wildcmp(s1 + 1, s2 + 1));
-    }
-    return (0);
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*' || *s1 == '\0')
+			return (wildcmp(s1, s2 + 1));
+		else
+			return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
+
